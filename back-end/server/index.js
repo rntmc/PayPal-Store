d84791c561722240a78036932e509eb76a418866
renato <renatocarrino@gmail.com>
2024-03-11 15:58:55 -0300

    create SERVER_URL

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as paypal from '../paypal-api.js';
 import cors from 'cors'
 const PORT = 8888;
+const SERVER_URL = "https://paypal-store-1.onrender.com";
 
 const app = express();
 
@@ -31,6 +32,6 @@ app.post("/my-server/capture-paypal-order", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Node server listening at http://localhost:${PORT}`);
+app.listen(SERVER_URL, () => {
+  console.log(`Server listening at ${SERVER_URL}`);
 });
